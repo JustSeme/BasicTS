09626d88ddfc711a902d126474f25a67552487fd
JustSemen <semyn03@mail.ru>
2022-12-26 14:45:20 +0600

    compile ts

--- 02/script.js
+++ 02/script.js
@@ -22,3 +22,39 @@ console.log(Directions.Up); // 0
 console.log(Directions.Down); // 1
 console.log(Directions.Left); // 2
 console.log(Directions.Right); // 3
+// or
+var DirectionsIndex;
+(function (DirectionsIndex) {
+    DirectionsIndex[DirectionsIndex["Up"] = 2] = "Up";
+    DirectionsIndex[DirectionsIndex["Down"] = 4] = "Down";
+    DirectionsIndex[DirectionsIndex["Left"] = 6] = "Left";
+    DirectionsIndex[DirectionsIndex["Right"] = 7] = "Right";
+})(DirectionsIndex || (DirectionsIndex = {}));
+console.log(DirectionsIndex.Up); // 2
+console.log(DirectionsIndex.Down); // 4
+console.log(DirectionsIndex.Left); // 6
+console.log(DirectionsIndex.Right); // 7
+// Never type
+// func return Error
+var msg = 'hello';
+var error = function (msg) {
+    throw new Error(msg);
+};
+// func infinite loop
+var infiniteLoop = function () {
+    while (true) {
+    }
+};
+// Object type
+var create = function (o) { };
+// create(1) error
+// create('42') error
+create({ obj: 1 });
+create(null);
+// Multiple types for one value
+var id;
+id = 10;
+id = '42';
+var nameVariable;
+nameVariable = '42';
+// nameVariable = 42 error
